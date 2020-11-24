9f7e6b66140fbb081187a0180652316c2a5f790d
Husam <husamcco@gmail.com>
2020-11-24 20:12:06 +0200

    Initial commit

---
+++ src/main/java/org/example/Customer.java
@@ -0,0 +1,53 @@
+package org.example;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String fName;
+    private String lName;
+    private String email;
+
+    public Customer(String fName, String lName, String email) {
+        this.fName = fName;
+        this.lName = lName;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
